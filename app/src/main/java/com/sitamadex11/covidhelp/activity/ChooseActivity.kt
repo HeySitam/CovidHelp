8b1efb5c6a1ce80c625b6c459f04e6cf2874c1f1
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-06-01 23:22:48 +0530

    Choose Activity Ui Update

--- app/src/main/java/com/sitamadex11/covidhelp/activity/ChooseActivity.kt
+++ app/src/main/java/com/sitamadex11/covidhelp/activity/ChooseActivity.kt
@@ -3,16 +3,13 @@ package com.sitamadex11.covidhelp.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.sitamadex11.covidhelp.R
-import kotlinx.android.synthetic.main.activity_choose.*
+import kotlinx.android.synthetic.main.activity_choose_demo.*
 
 class ChooseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose)
+        setContentView(R.layout.activity_choose_demo)
         cvNeedHelp.setOnClickListener {
             val intent = Intent(this, HelpActivity::class.java)
             startActivity(intent)
@@ -25,8 +22,12 @@ class ChooseActivity : AppCompatActivity() {
             val intent = Intent(this, VolunteerActivity::class.java)
             startActivity(intent)
         }
-        cvCowin.setOnClickListener {
+        cvVaccine.setOnClickListener {
             val intent = Intent(this, VaccineStatusActivity::class.java)
+            startActivity(intent)
+        }
+        cvCovidTracker.setOnClickListener{
+            val intent = Intent(this, CovidTrackerActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/sitamadex11/covidhelp/activity/CovidTrackerActivity.kt
@@ -0,0 +1,10 @@
+package com.sitamadex11.covidhelp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CovidTrackerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
