b6f73a2fd6cc2db156884c5e53094be5dd54332b
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-06-03 22:25:26 +0530

    navigation drawer added

--- app/src/main/java/com/sitamadex11/covidhelp/activity/ChooseActivity.kt
+++ app/src/main/java/com/sitamadex11/covidhelp/activity/ChooseActivity.kt
@@ -2,14 +2,21 @@ package com.sitamadex11.covidhelp.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.sitamadex11.covidhelp.R
-import kotlinx.android.synthetic.main.activity_choose_demo.*
+import kotlinx.android.synthetic.main.activity_choose.*
 
-class ChooseActivity : AppCompatActivity() {
+class ChooseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_demo)
+        setContentView(R.layout.activity_choose)
+        init()
         cvNeedHelp.setOnClickListener {
             val intent = Intent(this, HelpActivity::class.java)
             startActivity(intent)
@@ -26,9 +33,25 @@ class ChooseActivity : AppCompatActivity() {
             val intent = Intent(this, VaccineStatusActivity::class.java)
             startActivity(intent)
         }
-        cvCovidTracker.setOnClickListener{
+        cvCovidTracker.setOnClickListener {
             val intent = Intent(this, CovidTrackerActivity::class.java)
             startActivity(intent)
         }
+        hamburger.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    private fun init() {
+        drawerLayout = findViewById(R.id.drawerLayout)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.viewUserProfile -> {
+                Toast.makeText(this, "Let's view user profile", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/com/sitamadex11/covidhelp/fragments/ViewVolunteerFragment.kt
+++ app/src/main/java/com/sitamadex11/covidhelp/fragments/ViewVolunteerFragment.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.core.app.ActivityCompat
@@ -16,20 +17,33 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.Gson
 import com.sitamadex11.covidhelp.R
 import com.sitamadex11.covidhelp.activity.ChooseActivity
 import com.sitamadex11.covidhelp.adapter.VLAdapter
 import com.sitamadex11.covidhelp.adapter.VolunteerListAdapter
+import com.sitamadex11.covidhelp.model.District
+import com.sitamadex11.covidhelp.model.DistrictItems
+import com.sitamadex11.covidhelp.model.State
 import com.sitamadex11.covidhelp.model.VolunteerDetailsModel
+import com.sitamadex11.covidhelp.util.Constants
+import kotlinx.android.synthetic.main.fragment_view_volunteer.*
 
 
 class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
     lateinit var btnAddVolunteer: FloatingActionButton
     lateinit var rvVol: RecyclerView
     lateinit var adapter: VolunteerListAdapter
+    lateinit var requestOueue: RequestQueue
     val allVolunteers = ArrayList<VolunteerDetailsModel>()
+    val state_list = java.util.ArrayList<String>()
+    val district_list = java.util.ArrayList<DistrictItems>()
+    val district_name_list = java.util.ArrayList<String>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,18 +52,20 @@ class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
         val view = inflater.inflate(R.layout.fragment_view_volunteer, container, false)
         init(view)
         click()
-        addItemToList()
         callBack()
+        stateJsonParse()
         return view
     }
-    private fun callBack(){
-        val callBack=object : OnBackPressedCallback(true){
+
+    private fun callBack() {
+        val callBack = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                startActivity(Intent(requireContext(),ChooseActivity::class.java))
+                requireActivity().finish()
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(callBack)
     }
+
     private fun click() {
         btnAddVolunteer.setOnClickListener(this)
     }
@@ -59,6 +75,7 @@ class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
         rvVol = view.findViewById(R.id.rvVolunteer)
         rvVol.layoutManager = LinearLayoutManager(context)
         adapter = VolunteerListAdapter(requireContext(), this)
+        requestOueue = Volley.newRequestQueue(requireContext())
     }
 
     override fun onClick(v: View?) {
@@ -79,6 +96,7 @@ class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
     }
 
     private fun addItemToList() {
+        allVolunteers.clear()
         FirebaseFirestore.getInstance().collection("volunteers").get()
             .addOnSuccessListener { queryDocumentSnapshots ->
                 for (snapshot in queryDocumentSnapshots) {
@@ -88,15 +106,24 @@ class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
                     val phoneS: String? = snapshot.getString("phone")
                     val org = snapshot.getString("organization")
                     // val phone=Integer.parseInt(phoneS!!)
-                    allVolunteers.add(VolunteerDetailsModel(name, phoneS, state, district, org))
+                    if (etVolState.text.toString() == state && etVolDistrict.text.toString() == district) {
+                        allVolunteers.add(VolunteerDetailsModel(name, phoneS, state, district, org))
+                    }
                     // Log.d("ref_check", img.toString())
                 }
                 adapter.updateList(allVolunteers)
+                if (allVolunteers.isNotEmpty())
+                    txtVolInfo.visibility = View.GONE
+                else {
+                    txtVolInfo.visibility = View.VISIBLE
+                    txtVolInfo.text = "Sorry!! No Volunteer Found in your area"
+                }
                 rvVol.adapter = adapter
             }.addOnFailureListener { e ->
-            Log.d("EXPLOREFRAGMENT", "onFailure: " + e.message)
-            Toast.makeText(context, "Something went wrong...", Toast.LENGTH_SHORT).show()
-        }
+                Log.d("EXPLOREFRAGMENT", "onFailure: " + e.message)
+                txtVolInfo.text = "Can't Fetch Data"
+                Toast.makeText(context, "Something went wrong...", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun checkPermission(): Boolean {
@@ -114,27 +141,6 @@ class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
         )
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 200) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(
-                    requireActivity().applicationContext,
-                    "permission granted.",
-                    Toast.LENGTH_SHORT
-                ).show();
-            else
-                Toast.makeText(
-                    requireActivity().applicationContext,
-                    "permission denied.",
-                    Toast.LENGTH_SHORT
-                ).show();
-        }
-    }
 
     override fun onCallBtnClicked(phone: String) {
         if (!checkPermission()) {
@@ -144,5 +150,57 @@ class ViewVolunteerFragment : Fragment(), View.OnClickListener, VLAdapter {
             callIntent.data = Uri.parse("tel:$phone")
             startActivity(callIntent)
         }
+    }
+
+    private fun stateJsonParse() {
+        val url = Constants.STATE_URL
+        val stateString = StringRequest(url, { str ->
+            val state_class = Gson().fromJson(str, State::class.java)
+            for (i in state_class.states.indices) {
+                state_list.add(state_class.states[i].state_name)
+            }
+            val stateAdapter =
+                ArrayAdapter(requireContext(), R.layout.dropdown_item, state_list)
+            etVolState.setAdapter(stateAdapter)
+            etVolState.setOnItemClickListener { _, _, position, _ ->
+                Toast.makeText(requireContext(), state_list[position], Toast.LENGTH_SHORT).show()
+                etVolDistrict.isEnabled = true
+                district_list.clear()
+                district_name_list.clear()
+                districtJsonParse(position)
+            }
+
+            Log.d("chk_state", str)
+        }, {
+            Toast.makeText(requireContext(), "something went wrong", Toast.LENGTH_SHORT).show()
+        })
+        requestOueue.add(stateString)
+
+    }
+
+    private fun districtJsonParse(position: Int) {
+        val url = "${Constants.DISTRICT_URL}${position}"
+        Log.d("chk_url", url)
+        val districtString = StringRequest(url, { str ->
+            val district_class = Gson().fromJson(str, District::class.java)
+            for (i in district_class.districts!!.indices) {
+                district_list.add(district_class.districts[i]!!)
+                district_name_list.add(district_class.districts[i]!!.district_name!!)
+            }
+            val stateAdapter =
+                ArrayAdapter(requireContext(), R.layout.dropdown_item, district_name_list)
+            etVolDistrict.apply {
+                setAdapter(stateAdapter)
+                setOnItemClickListener { _, _, position, _ ->
+                    txtVolInfo.visibility = View.VISIBLE
+                    txtVolInfo.text = "Fetching data..."
+                    addItemToList()
+                }
+            }
+            Log.d("chk_state", str)
+        }, {
+            Toast.makeText(requireContext(), "something went wrong", Toast.LENGTH_SHORT).show()
+        })
+        requestOueue.add(districtString)
     }
 }
