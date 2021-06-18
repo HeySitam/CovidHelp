b2893a5d0604f1616907b58d5561fb345f522072
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-06-19 00:13:26 +0530

    all jpg removed

--- app/src/main/java/com/sitamadex11/covidhelp/activity/ChooseActivity.kt
+++ app/src/main/java/com/sitamadex11/covidhelp/activity/ChooseActivity.kt
@@ -1,9 +1,12 @@
 package com.sitamadex11.covidhelp.activity
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -13,14 +16,18 @@ import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.sitamadex11.covidhelp.R
+import com.sitamadex11.covidhelp.fragments.AboutUsFragment
 import com.sitamadex11.covidhelp.fragments.HomeFragment
 import com.sitamadex11.covidhelp.fragments.ViewProfileFragment
 import com.sitamadex11.covidhelp.worker.CovidTrackerWorker
 import kotlinx.android.synthetic.main.activity_choose.*
 import java.util.concurrent.TimeUnit
+
 
 class ChooseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var drawerLayout: DrawerLayout
@@ -28,6 +35,9 @@ class ChooseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
     lateinit var firebaseAuth: FirebaseAuth
     lateinit var txtUserName: TextView
     lateinit var fragmentManager: FragmentManager
+    lateinit var btnSend: MaterialButton
+    lateinit var btnCancel: MaterialButton
+    lateinit var etSendEmail: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose)
@@ -65,6 +75,43 @@ class ChooseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
                 val intent =  Intent(this,VolunteerActivity::class.java)
                 startActivity(intent)
             }
+            R.id.aboutUs ->{
+                fragmentTransaction(AboutUsFragment())
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            R.id.contactUs ->{
+                drawerLayout.closeDrawer(GravityCompat.START)
+                val customLayout = layoutInflater
+                    .inflate(
+                        R.layout.dialog_email, null
+                    )
+                msgInit(customLayout)
+                val builder = AlertDialog.Builder(this)
+                builder.setView(customLayout)
+                val dialog = builder.create()
+                btnCancel.setOnClickListener{
+                    dialog.hide()
+                }
+                dialog.show()
+                btnSend.setOnClickListener {
+                    if (etSendEmail.text.isNullOrEmpty()) {
+                        etSendEmail.error = "Please enter some text message"
+                    } else {
+                        val email = Intent(Intent.ACTION_SENDTO)
+                        val to ="sitamadex11@gmail.com"
+                        val subject = "Covid-Help FeedBack"
+                        val intent = Intent(Intent.ACTION_SENDTO)
+                         intent.data = Uri.parse("mailto:$to") // only email apps should handle this
+                        intent.putExtra(Intent.EXTRA_EMAIL, to)
+                        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                        intent.putExtra(Intent.EXTRA_TEXT, etSendEmail.text.toString())
+                        if (intent.resolveActivity(packageManager) != null) {
+                            startActivity(intent)
+                        }
+                        etSendEmail.error = null
+                    }
+                }
+            }
             R.id.logout -> {
                 drawerLayout.closeDrawer(GravityCompat.START)
                 firebaseAuth.signOut()
@@ -72,6 +119,12 @@ class ChooseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
             }
         }
         return true
+    }
+
+    private fun msgInit(v: View?) {
+       btnSend =v!!.findViewById(R.id.btnSendEmail)
+       btnCancel =v.findViewById(R.id.btnCancelEmail)
+       etSendEmail =v.findViewById(R.id.etSendEmail)
     }
 
     override fun onBackPressed() {
