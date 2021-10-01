bb8c4fafee74a9e5fa9d14ebb090c9d81ad98f52
Akash Kumar Sen <71623442+Akash-Kumar-Sen@users.noreply.github.com>
2021-10-01 15:53:37 +0530

    Login screen UI update (#11)
    Updated the UI of login screen.

--- Extension Features/extention1.kt
+++ Extension Features/extention1.kt
@@ -1,1 +1,13 @@
 
+fun main(args: Array<String>) {
+    val num = 6
+    val factorial = multiplyNumbers(num)
+    println("Factorial of $num = $factorial")
+}
+
+fun multiplyNumbers(num: Int): Long {
+    if (num >= 1)
+        return num * multiplyNumbers(num - 1)
+    else
+        return 1
+}
