1c0e77dc9b659045d66004be1fbca3d7ac15b9ba
Sk Feejan <82304450+SkFeejan@users.noreply.github.com>
2021-10-03 18:15:16 +0530

    Create extension12.kt (#43)

---
+++ Extension Features/extension12.kt
@@ -0,0 +1,36 @@
+package com.sitamadex11.CovidHelp.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.sitamadex11.CovidHelp.R
+import com.sitamadex11.CovidHelp.fragments.CityWiseFragment
+import com.sitamadex11.CovidHelp.fragments.StateWiseFragment
+import kotlinx.android.synthetic.main.help_activity.*
+
+class HelpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.help_activity)
+        bottom_navigation.setItemSelected(R.id.stateWise, true)
+        fragmentTransaction(StateWiseFragment())
+        bottom_navigation.setOnItemSelectedListener {
+            lateinit var fragment: Fragment
+            when (it) {
+                R.id.stateWise -> {
+                    fragment = StateWiseFragment()
+                }
+                R.id.cityWise -> {
+                    fragment = CityWiseFragment()
+                }
+            }
+            fragmentTransaction(fragment)
+        }
+    }
+
+    fun fragmentTransaction(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment).commit()
+    }
+}
