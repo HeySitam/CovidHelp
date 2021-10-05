bec004a47ea6f0d82aa62c223f9bb282a0910190
cdhiraj40 <chauhandhiraj40@gmail.com>
2021-10-05 15:44:19 +0530

    closing keyboard when clicked in background

--- app/src/main/java/com/sitamadex11/CovidHelp/activity/VolunteerActivity.kt
+++ app/src/main/java/com/sitamadex11/CovidHelp/activity/VolunteerActivity.kt
@@ -1,7 +1,12 @@
 package com.sitamadex11.CovidHelp.activity
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import com.sitamadex11.CovidHelp.R
 import com.sitamadex11.CovidHelp.fragments.ViewVolunteerFragment
@@ -19,5 +24,20 @@ class VolunteerActivity : AppCompatActivity() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.flVolunteer, fragment).commit()
+    }
+    // this  lets keyboard close when clicked in background
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
