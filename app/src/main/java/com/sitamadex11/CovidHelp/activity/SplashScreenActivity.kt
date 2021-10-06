b8da041839d8c5722de507596c7bc7a5e393b2a5
Siddharth Singh <72121163+SiddyDevelops@users.noreply.github.com>
2021-10-06 20:34:25 +0530

    UI Home Page (#75)

--- app/src/main/java/com/sitamadex11/CovidHelp/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/sitamadex11/CovidHelp/activity/SplashScreenActivity.kt
@@ -1,16 +1,28 @@
 package com.sitamadex11.CovidHelp.activity
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import com.sitamadex11.CovidHelp.R
+import android.view.WindowManager
+
+
+
 
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val window: Window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        val colorCodeDark: Int = Color.parseColor("#F4B185")
+        window.setStatusBarColor(colorCodeDark)
+
         Handler().postDelayed(Runnable {
             val i = Intent(
                 this,
