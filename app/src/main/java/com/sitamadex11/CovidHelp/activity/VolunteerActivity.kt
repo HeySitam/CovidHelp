983d83b9d9a07e40f8c5817c515a9a726846d873
Dhiraj Chauhan <Chauhandhiraj40@gmail.com>
2021-10-09 20:08:18 +0530

    Old Closing Keyboard approach is causing Problems in Login/Sign up screen (#84)
    * solved #23/74
    
    * closing Keyboard when clicked in background proper approach
    
    * closing Keyboard when clicked in background proper approach
    
    * Update VolunteerActivity.kt

--- app/src/main/java/com/sitamadex11/CovidHelp/activity/VolunteerActivity.kt
+++ app/src/main/java/com/sitamadex11/CovidHelp/activity/VolunteerActivity.kt
@@ -3,6 +3,7 @@ package com.sitamadex11.CovidHelp.activity
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -26,18 +27,34 @@ class VolunteerActivity : AppCompatActivity() {
             .replace(R.id.flVolunteer, fragment).commit()
     }
     // this  lets keyboard close when clicked in background
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is EditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm = getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(v.windowToken, 0)
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (ev.action == MotionEvent.ACTION_UP) {
+            val view = currentFocus
+            if (view != null) {
+                val consumed = super.dispatchTouchEvent(ev)
+                val viewTmp = currentFocus
+                val viewNew: View = viewTmp ?: view
+                if (viewNew == view) {
+                    val rect = Rect()
+                    val coordinates = IntArray(2)
+                    view.getLocationOnScreen(coordinates)
+                    rect[coordinates[0], coordinates[1], coordinates[0] + view.width] =
+                        coordinates[1] + view.height
+                    val x = ev.x.toInt()
+                    val y = ev.y.toInt()
+                    if (rect.contains(x, y)) {
+                        return consumed
+                    }
+                } else {
+                    return consumed
                 }
+                val inputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(viewNew.windowToken, 0)
+                viewNew.clearFocus()
+                return consumed
             }
         }
-        return super.dispatchTouchEvent(event)
+        return super.dispatchTouchEvent(ev)
     }
 }
